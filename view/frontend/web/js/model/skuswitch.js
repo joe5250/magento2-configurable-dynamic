624e1c0888df832ee38c176ed4501435707a034c
klg <k.langenberg@imi.de>
2019-03-27 18:01:57 +0100

    Started adding css

--- view/frontend/web/js/model/skuswitch.js
+++ view/frontend/web/js/model/skuswitch.js
@@ -10,20 +10,24 @@ define([
 	return function(targetModule){
 		var reloadPrice = targetModule.prototype._reloadPrice;
 		targetModule.prototype.dynamic = {};
+		targetModule.prototype.dynamicCssClass = {};
 
 		$('[data-dynamic]').each(function(){
 			var code = $(this).data('dynamic');
 			var value = $(this).html();
+			var cssClass = $(this).attr('class');
 
 			targetModule.prototype.dynamic[code] = value;
+			targetModule.prototype.dynamicCssClass[code] = cssClass;
 		});
 
 		var reloadPriceWrapper = wrapper.wrap(reloadPrice, function(original){
 			var dynamic = this.options.spConfig.dynamic;
-			console.log(dynamic);
+			console.log(dynamic, this.dynamic);
 			for (var code in dynamic){
 				if (dynamic.hasOwnProperty(code)) {
 					var value = "";
+					var cssClass = '';
 					var $placeholder = $('[data-dynamic='+code+']');
 
 					if(!$placeholder.length) {
@@ -32,9 +36,13 @@ define([
 
 					if(this.simpleProduct){
 						value = this.options.spConfig.dynamic[code][this.simpleProduct].value;
+						cssClass = this.options.spConfig.dynamic[code][this.simpleProduct].cssClass == undefined ? '' : this.options.spConfig.dynamic[code][this.simpleProduct].cssClass;
 					} else {
 						value = this.dynamic[code];
+						cssClass = this.dynamicCssClass[code];
 					}
+
+					console.log(value, cssClass, dynamic[code]);
 
 					$placeholder.html(value);
 				}
