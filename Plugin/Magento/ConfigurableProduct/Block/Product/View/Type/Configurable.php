96f46353946f9ae5490b3304ac6d8f1480425c36
Galla, Daniel <d.galla@imi.de>
2019-08-09 17:54:20 +0200

    Add feature to show product attributes dynamically

--- Plugin/Magento/ConfigurableProduct/Block/Product/View/Type/Configurable.php
+++ Plugin/Magento/ConfigurableProduct/Block/Product/View/Type/Configurable.php
@@ -8,8 +8,18 @@
 
 namespace Andering\ConfigurableDynamic\Plugin\Magento\ConfigurableProduct\Block\Product\View\Type;
 
+use Andering\ConfigurableDynamic\Block\Product\View\Attributes;
+use Magento\Catalog\Model\Product;
+
 class Configurable
 {
+    private $layout;
+
+    public function __construct(\Magento\Framework\View\LayoutInterface $layout)
+    {
+        $this->layout = $layout;
+    }
+
     public function afterGetJsonConfig(\Magento\ConfigurableProduct\Block\Product\View\Type\Configurable $subject, $result) {
 
         $jsonResult = json_decode($result, true);
@@ -25,9 +35,31 @@ class Configurable
 					];
 				}
         	}
+
+            $jsonResult = $this->addSimpleAttributesBlock($simpleProduct, $jsonResult, $id);
         }
 
         $result = json_encode($jsonResult);
         return $result;
+    }
+
+    private function addSimpleAttributesBlock(Product $simpleProduct, $jsonResult, int $id)
+    {
+        $jsonResult['dynamic']['product_attributes'][$id] = [
+            'value' => $this->getProductAttributesBlockHtml($simpleProduct),
+        ];
+
+        return $jsonResult;
+    }
+
+    private function getProductAttributesBlockHtml(Product $product)
+    {
+        /** @var Attributes $originalBlock */
+        $originalBlock = $this->layout->getBlock('product.attributes');
+        /** @var Attributes $block */
+        $block = $this->layout->createBlock(Attributes::class, '', $originalBlock->getData());
+        $block->setProduct($product);
+
+        return $block->toHtml();
     }
 }
